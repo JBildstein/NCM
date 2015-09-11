43abc639e7f6b7c6a7739aace3f1dade53a82c5a
Johannes Bildstein <info@fotostein.at>
2015-09-11 11:37:05 +0200

    added ArrayHelper to easily create arrays for tests

---
+++ ColorManagerTests/Helper/ArrayHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace ColorManagerTests
+{
+    public static class ArrayHelper
+    {
+        public static T[] Concat<T>(params T[][] arrs)
+        {
+            var result = new T[arrs.Sum(t => t.Length)];
+            int offset = 0;
+            for (int i = 0; i < arrs.Length; i++)
+            {
+                arrs[i].CopyTo(result, offset);
+                offset += arrs[i].Length;
+            }
+            return result;
+        }
+        
+        public static T[] Fill<T>(T value, int length)
+        {
+            var result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = value;
+            }
+            return result;
+        }
+
+        public static string Fill(char value, int length)
+        {
+            return "".PadRight(length, value);
+        }
+    }
+}
